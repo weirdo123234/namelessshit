7eb79a95e1dcefc85259e8565a1f515e6fedd8c9
weirdo123234 <charlesarieger@icloud.com>
2024-09-21 00:46:33 -0600

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,135 @@
+local Library = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+local currentVersion = "1.0.0"
+local chat = game.TextChatService.TextChannels.RBXGeneral
+chat:SendAsync("Use Nameless S>H>I>T Roblox script for bypassing!")
+local Window = Fluent:CreateWindow({
+    Title = "Nameless Shit v" .. currentVersion,
+    SubTitle = "By Medium!",
+    TabWidth = 160,
+    Size = UDim2.fromOffset(580, 460),
+    Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
+    Theme = "Dark",
+    MinimizeKey = Enum.KeyCode.LeftControl -- Used when theres no MinimizeKeybind
+})
+local Shit = {
+    Main = Window:AddTab({ Title = "Swears", Icon = "" }),
+}
+Shit.Main:AddButton({
+    Title = "Let's shit in the toilet together at the same time!",
+    Description = "",
+    Callback = function()
+       chat:SendAsync("Let's <<s>>h<<i>>t<< in the toilet together at the same time!")
+    end
+})
+Shit.Main:AddButton({
+    Title = "Oh shit!",
+    Description = "",
+    Callback = function()
+        chat:SendAsync("Oh <<s>>h<<i>>t<<!")
+    end
+})
+Shit.Main:AddButton({
+    Title = "Let's play a game, I shit into your ass and you shit it back into mine!",
+    Description = "",
+    Callback = function()
+        chat:SendAsync("Let's play a game, I <<s>>h<<i>>t<< into your <<a>>s<<s>> and you <<s>>h>>i>>t<< it back into mine!")
+    end
+})
+Shit.Main:AddButton({
+    Title = "Fuck, I did some shit wrong!",
+    Description = "",
+    Callback = function()
+        chat:SendAsync("<<F>>u<<c>>k<<, I did some <<s>>h<<i>>t<< wrong!")
+    end
+})
+Shit.Main:AddButton({
+    Title = "*shits in ass*",
+    Description = "",
+    Callback = function()
+        chat:SendAsync("*s>h>i>t>s in a>s>s*")
+    end
+})
+Shit.Main:AddButton({
+    Title  = "Oh shit, bro got that huge cock!",
+    Description = "",
+    Callback = function()
+        chat:SendAsync("Oh <<s>>h>>i>>t<<, bro got that huge <<c<>o<<c<>>k!")
+    end
+})
+Shit.Main:AddButton({
+    Title = "Can I see your cock?",
+    Description = "",
+    Callback = function()
+        chat:SendAsync("Can I see your <c>o>c>k>?")
+    end
+})
+Shit.Main:AddButton({
+    Title = "What The Fuck",
+    Description = "",
+    Callback = function()
+        chat:SendAsync("What the <f>u>c<k>!")
+    end
+})
+Shit.Main:AddButton({
+    Title = "Fuck You!",
+    Description = "",
+    Callback = function()
+        chat:SendAsync("F<u>c>k< you!")
+    end
+})
+Shit.Main:AddButton({
+    Title = "My Penis Is Going To Have Yellow Liquid Come Out",
+    Description = "",
+    Callback = function()
+        chat:SendAsync("My p<e>n<i<s> is going to have yellow liquid come out.")
+    end
+})
+Shit.Main:AddButton({
+    Title = ";fly, oh shit I cant fly!",
+    Description = "",
+    Callback = function()
+        chat:SendAsync(";fly, oh s<h>i<t> I cant fly!")
+    end
+})
+Shit.Main:AddButton({
+    Title = "Oh well, that's shitty!",
+    Description = "",
+    Callback = function()
+        chat:SendAsync("Oh well, that's s>h>i>t>t>y>!")
+    end
+})
+Shit.Main:AddButton({
+    Title = "Welcome to shitty hotels, how can I help you?",
+    Description = "",
+    Callback = function()
+        chat:SendAsync("Welcome to s>h>i>t>t>y> hotels, how can I help you?")
+    end
+})
+Shit.Main:AddButton({
+    Title = "Did You Know Skibidi Toilet Has A Huge Cock",
+    Description = "",
+    Callback = function()
+        chat:SendAsync("Did you know Skibidi Toilet has a huge <c>o>c>k>?")
+    end
+})
+Shit.Main:AddButton({
+    Title = "Shut The Fuck Up",
+    Description = "",
+    Callback = function()
+        chat:SendAsync("🅢🅗🅤🅣 🅣🅗🅔 🅕🅤🅒🅚 🅤🅟!")
+    end
+})
+Shit.Main:AddButton({
+    Title = "Wiggling My Dick",
+    Description = "",
+    Callback = function()
+        chat:SendAsync("Wiggling my <d>i>c>k>!")
+    end
+})
+Shit.Main:AddButton({
+    Title = "Can I Kiss Your Armpit?",
+    Description = "",
+    Callback = function()
+        chat:SendAsync("Can I k>i>s>s> your a>r>m>p>i>t>?")
+    end
+})
